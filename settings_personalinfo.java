430be7f9decc51b9b4b858a2d034fd18c05f570d
saniah31 <118789064+saniah31@users.noreply.github.com>
2022-11-23 17:11:09 -0600

    Create settings_personalinfo.java

---
+++ settings_personalinfo.java
@@ -0,0 +1,14 @@
+package com.example.bidsplashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class settings_personalinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_personalinfo);
+    }
+}
